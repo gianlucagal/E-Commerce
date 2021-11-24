a008e80181bb4e81c9b6e0921e9c2e218684687d
MacBook Air <macbookair@gianluca.homenet.telecomitalia.it>
2021-11-24 15:54:33 +0100

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import HomePage from './Component/homepage.component';
+
+function App() {
+  return (
+    <div>
+      <HomePage />
+    </div>
+  );
+}
+
+export default App;
