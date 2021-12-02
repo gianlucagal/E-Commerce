25a8ffb160a62a71f91696f252480ab3d98338e4
gianlucagal <gallonigianluca@hotmail.com>
2021-12-02 14:47:22 +0100

    Adding Directory

--- src/Component/Directory/directory.component.jsx
+++ src/Component/Directory/directory.component.jsx
@@ -1,42 +1,51 @@
 import React from "react";
 
+import MenuItem from '../MenuItem/menu-item.component';
+
 class Directory extends React.Component {
   constructor () {
     super();
 
-    this.state = [
-      {
-        title: 'hats',
-        iamgeUrl: 'https://i.ibb.co/cvpntL1/hats.png',
-        id: 1
-      },
-      {
-        title: 'jackets',
-        imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
-        id: 2
-      },
-      {
-        title: 'sneacker',
-        imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
-        id: 3
-      },
-      {
-        title: 'womens',
-        imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
-        id: 4
-      },
-      {
-        title: 'mens',
-        imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
-        id: 5
-      }
-    ]
+    this.state = {
+        sections: [
+            {
+                title: 'hats',
+                iamgeUrl: 'https://i.ibb.co/cvpntL1/hats.png',
+                id: 1
+            },
+            {
+                title: 'jackets',
+                imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
+                id: 2
+            },
+            {
+                title: 'sneacker',
+                imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
+                id: 3
+            },
+            {
+                title: 'womens',
+                imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
+                id: 4
+            },
+            {
+                title: 'mens',
+                imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
+                id: 5
+            }
+        ]
+    }
   }
 
-
-  return (
-    
-  )
+  render() {
+    return (
+      <div className='directory-menu'>
+        {this.state.sections.map(({title, imageUrl, id}) => (
+          <MenuItem key={id} title={title} />
+        ))}
+      </div>
+    )
+  }
 };
 
 export default Directory;
