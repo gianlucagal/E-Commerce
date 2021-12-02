0deab57c21a71820e2d518eef951842e0ce03556
gianlucagal <gallonigianluca@hotmail.com>
2021-12-02 14:32:25 +0100

    Creating first components and homepage

---
+++ src/Component/Directory/directory.component.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+
+class Directory extends React.Component {
+  constructor () {
+    super();
+
+    this.state = [
+      {
+        title: 'hats',
+        iamgeUrl: 'https://i.ibb.co/cvpntL1/hats.png',
+        id: 1
+      },
+      {
+        title: 'jackets',
+        imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
+        id: 2
+      },
+      {
+        title: 'sneacker',
+        imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
+        id: 3
+      },
+      {
+        title: 'womens',
+        imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
+        id: 4
+      },
+      {
+        title: 'mens',
+        imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
+        id: 5
+      }
+    ]
+  }
+
+
+  return (
+    
+  )
+};
+
+export default Directory;
