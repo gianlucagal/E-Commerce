9715134f95e0d6e16036a74d6ad190343607f248
Gianluca Galloni <gallonigianluca@hotmail.com>
2022-02-28 13:57:39 +0100

    Ending style

--- src/Component/HomePage/homepage.component.jsx
+++ src/Component/HomePage/homepage.component.jsx
@@ -1,47 +1,11 @@
 import React from "react";
 import './homepage.style.scss';
+import Directory from '../Directory/directory.component';
 
 const HomePage = () => {
   return (
     <div className= 'homepage' >
-      <div className='directory-menu'>
-        <div className='menu-item'>
-          <div className='content'>
-            <h1 className='title'>HATS</h1>
-            <h1 className='subtitle'>SHOP NOW</h1>
-          </div>
-        </div>
-        <div className='menu-item'>
-          <div className='content'>
-            <h1 className='title'>JACKETS</h1>
-            <h1 className='subtitle'>SHOP NOW</h1>
-          </div>
-        </div>
-        <div className='menu-item'>
-          <div className='content'>
-            <h1 className='title'>SNEAKERS</h1>
-            <h1 className='subtitle'>SHOP NOW</h1>
-          </div>
-        </div>
-        <div className='menu-item'>
-          <div className='content'>
-            <h1 className='title'>WOMENS</h1>
-            <h1 className='subtitle'>SHOP NOW</h1>
-          </div>
-        </div>
-        <div className='menu-item'>
-          <div className='content'>
-            <h1 className='title'>MENS</h1>
-            <h1 className='subtitle'>SHOP NOW</h1>
-          </div>
-        </div>
-        <div className='menu-item'>
-          <div className='content'>
-            <h1 className='title'>HATS</h1>
-            <h1 className='subtitle'>SHOP NOW</h1>
-          </div>
-        </div>
-      </div>
+      <Directory />
     </div>
   )
 }
